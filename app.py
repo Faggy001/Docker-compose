55c3871871c641b9b7db9dce65d74f281bc787ad
faggy001 <faggy29@gmail.com>
2025-06-11 13:18:03 +0100

    comma removed from app.py line 12

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_DB = os.getenv("POSTGRES_DB",)
+POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST") 
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
